52a465d353f4ed5a5e7b790cd5db24e74d791140
milittle <mizeshuang@gmail.com>
2022-03-27 12:07:35 +0800

    lazytest

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    fn f() -> &'static str {
+        "hello, "
+    }
+    let f = || -> &'static str { "world\n" };
+    println!("{}", crate::m!());
+    #[macro_export]
+    macro_rules! m {
+        () => {
+            f()
+        };
+    }
+    println!("{}", crate::m!());
 }
